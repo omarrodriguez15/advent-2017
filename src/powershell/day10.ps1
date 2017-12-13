f40e8b21735b5a45afcb05c58fd044778d4d3110
rr2d2 <rodriguezomar91@gmail.com>
2017-12-13 00:01:24 -0600

    Refactored part1 and finished part 2 day10

--- src/powershell/day10.ps1
+++ src/powershell/day10.ps1
@@ -1,10 +1,11 @@
 function KnotHash{
    Param(
       $lengths,
-      $stdList
+      $stdList,
+      $currPos = 0,
+      $skipSize = 0
    )
 
-   $currPos = 0
    for ($i=0;$i -lt $lengths.Count;$i++) {
       $length = $lengths[$i]
       
@@ -33,30 +34,101 @@ function KnotHash{
          $stdList[$j] = $reverseList[$ndx]
          $ndx++
       }
-      # skipSize = $i
-      $currPos = ($currPos + $i + $length) % $stdList.Count
+      
+      $currPos = ($currPos + $skipSize + $length) % $stdList.Count
+      $skipSize++
    }
-   $stdList
+   $properties = @{
+      List=$stdList;
+      Position=$currPos;
+      SkipSize=$skipSize;
+   }
+   New-Object PSObject –Property $properties
 }
 
+function ConvertLengthsToAscii{
+   Param(
+      $lengthsStr
+   )
+   $asciiMap = @{','=44;'0'=48;'1'=49;'2'=50;'3'=51;'4'=52;'5'=53;'6'=54;'7'=55;'8'=56;'9'=57;}
+   $suffix = @(17, 31, 73, 47, 23)
+   $newLengths = @()
 
-#Part 1
-$lengths = @(147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70)
+   $lengthsStr.ToCharArray() | %{
+      $newLengths += $asciiMap[[string]$_]
+   }
+   $suffix | %{
+      $newLengths += $_
+   }
+
+   $newLengths
+}
+
+function SparseHash{
+   Param(
+      $stdList,
+      $lengths
+   )
+   $rounds = 64
+   $pos = 0
+   $skipSize = 0
+   $list = $stdList
+   for($i=0;$i -lt 64; $i++){
+      
+      $result = KnotHash $newLengths $list $pos $skipSize
+
+      $list = $result.List
+      $pos = $result.Position
+      $skipSize = $result.SkipSize
+   }
+   $result
+}
+
+function DenseHash {
+   Param(
+      $list
+   )
+   $newList = @()
+   while($list.Count -gt 0){
+      $chunk = $list[0..15]
+      $list = $list[16..$list.Count]
+      $result = 0
+      $chunk | %{
+         $result = $result -bxor $_
+      }
+      $newList += $result
+   }
+   $newList
+}
+
+function DecToHex{
+   Param(
+      $list
+   )
+   $str = ""
+   $list | %{
+      $hexStr = [Convert]::ToString($_, 16)
+      if ($hexStr.length -lt 2){
+         $hexStr = "{0}{1}" -f 0, $hexStr
+      }
+      $str += $hexStr
+   }
+   $str
+}
+
 $stdList = (0..255)
-$result = KnotHash $lengths $stdList
-$product = $result[0] * $result[1]
-"stdList= $result product= $product"
+
+# Part 1 37230
+$lengths = @(147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70)
+$list = (KnotHash $lengths $stdList).List
+$product = $list[0] * $list[1]
+"`r`nPart 1 product = $product`r`n"
 
 # part2
-# $lengthsStr = "147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70"
-# $asciiMap = @{','=44;'0'=48;'1'=49;'2'=50;'3'=51;'4'=52;'5'=53;'6'=54;'7'=55;'8'=56;'9'=57;}
-# $suffix = @(17, 31, 73, 47, 23)
-# $newLengths = @()
+$lengthsStr = "147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70"
+$newLengths = ConvertLengthsToAscii $lengthsStr
 
-# $lengthsStr.ToCharArray() | %{
-#    $newLengths += $asciiMap[[string]$_]
-# }
-# $suffix | %{
-#    $newLengths += $_
-# }
-
+$sh = SparseHash $stdList $newLengths
+$dh = DenseHash $sh.List
+$hashStr = DecToHex $dh
+"Part 2 Hash String = $hashStr`r`n"
